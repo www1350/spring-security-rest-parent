a934e01a9ea30811512db55cf4a3f5bc92095cd3
www1350 <www_1350@163.com>
2016-10-12 17:13:41 +0800

    @update controller permit

--- spring-security-login-custom/src/main/java/com/absurd/web/IndexController.java
+++ spring-security-login-custom/src/main/java/com/absurd/web/IndexController.java
@@ -1,5 +1,8 @@
 package com.absurd.web;
 
+import com.absurd.security.SecurityRole;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(value = "/api")
 public class IndexController {
+    @PreAuthorize(value = "hasAuthority('ROLE_ADMIN')")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ResponseBody
     public Integer findOne(@PathVariable("id") final Long id) {
